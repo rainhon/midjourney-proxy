62a797cfdf10c421a67bc5fee7edd6584d7e8aa9
rain hon <240918304@qq.com>
2024-07-01 15:25:00 +0800

    Update WebMvcConfig.java
    跨域解决

--- src/main/java/spring/config/WebMvcConfig.java
+++ src/main/java/spring/config/WebMvcConfig.java
@@ -30,4 +30,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		}
 	}
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/**") // 所有接口
+	                .allowCredentials(true) // 是否发送 Cookie
+	                .allowedOriginPatterns("*") // 支持域
+	                .allowedMethods("GET", "POST", "PUT", "DELETE") // 支持方法
+	                .allowedHeaders("*")
+	                .exposedHeaders("*");
+	}
+
 }
